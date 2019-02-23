7b24c99fd1e88075558eeb7b613dad825243c0b0
Brent Kuhn <bkuhn@Brents-MacBook-Pro.local>
2019-02-22 20:47:11 -0500

    Created potential login screen

--- app/src/main/java/group1/spring19/cop4656/floridapoly/bander/MainActivity.java
+++ app/src/main/java/group1/spring19/cop4656/floridapoly/bander/MainActivity.java
@@ -2,6 +2,7 @@ package group1.spring19.cop4656.floridapoly.bander;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText userName = findViewById(R.id.u_name);
+        userName.setHint(getResources().getString(R.string.u_name));
+
+        final EditText pWord = findViewById(R.id.p_word);
+        pWord.setHint(getResources().getString(R.string.p_word));
     }
 }
