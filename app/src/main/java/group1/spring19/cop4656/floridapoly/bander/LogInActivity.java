688972812a652bc2593f651ad33c483340f585a7
Brent <bkuhn4669@flpoly.org>
2019-03-17 22:45:44 -0400

    Added create account and log-in functionality to the app

--- app/src/main/java/group1/spring19/cop4656/floridapoly/bander/LogInActivity.java
+++ app/src/main/java/group1/spring19/cop4656/floridapoly/bander/LogInActivity.java
@@ -1,16 +1,25 @@
 package group1.spring19.cop4656.floridapoly.bander;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity {
 
-    private EditText userName;
-    private EditText pWord;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private FirebaseAuth auth;
     private Button logIn;
     private Button createAccount;
 
@@ -19,9 +28,18 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        userName = findViewById(R.id.u_name);
-        pWord = findViewById(R.id.p_word);
-        //this sets up on click listener for the create Account button
+        // Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        // Check to see if a user is already logged in
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LogInActivity.this, MainActivity.class));
+            finish();
+        }
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        // this sets up on click listener for the create Account button
         createAccount = (Button) findViewById(R.id.createAccountButton);
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +53,33 @@ public class LogInActivity extends AppCompatActivity {
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToProfile();
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Please Enter an email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Please Enter a password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            // If login failed
+                            Toast.makeText(LogInActivity.this, "Authentication Failed" + task.getException(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+//                goToProfile();
 
             }
         });
@@ -50,9 +94,9 @@ public class LogInActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void goToProfile() {
-        Intent intent = new Intent(this, MainActivity.class);
-
-        startActivity(intent);
-    }
+//    public void goToProfile() {
+//        Intent intent = new Intent(this, MainActivity.class);
+//
+//        startActivity(intent);
+//    }
 }
--- app/src/main/java/group1/spring19/cop4656/floridapoly/bander/UsernamePasswordActivity.java
+++ app/src/main/java/group1/spring19/cop4656/floridapoly/bander/UsernamePasswordActivity.java
@@ -1,26 +1,38 @@
 package group1.spring19.cop4656.floridapoly.bander;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UsernamePasswordActivity extends AppCompatActivity {
 
-    private EditText userNameCreate;
-    private EditText pWordCreate;
-    private Button next;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private Button signUp;
     private Button cancel;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_username_password);
 
-        userNameCreate = findViewById(R.id.useNameCreateEditText);
-        pWordCreate = findViewById(R.id.passCreateEditText);
+        // Get the Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
         //this sets up on click listener for the create Account button
         cancel = (Button) findViewById(R.id.userNamPassCancelButton);
         cancel.setOnClickListener(new View.OnClickListener() {
@@ -31,12 +43,39 @@ public class UsernamePasswordActivity extends AppCompatActivity {
             }
         });
 
-        next = (Button) findViewById(R.id.useNamePassNextButton);
-        next.setOnClickListener(new View.OnClickListener() {
+        signUp = (Button) findViewById(R.id.userNamePassNextButton);
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToEditProfile();
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
 
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Please Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Please Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(UsernamePasswordActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()) {
+                            Toast.makeText(UsernamePasswordActivity.this, "Authentification failed " + task.getException(),  Toast.LENGTH_SHORT).show();
+                        } else{
+                            startActivity(new Intent(UsernamePasswordActivity.this, EditProfileActivity.class));
+                            finish();
+                        }
+                    }
+                });
             }
         });
 
@@ -45,12 +84,6 @@ public class UsernamePasswordActivity extends AppCompatActivity {
 
     public void goToLogin() {
         Intent intent = new Intent(this, LogInActivity.class);
-
-        startActivity(intent);
-    }
-
-    public void goToEditProfile() {
-        Intent intent = new Intent(this, EditProfileActivity.class);
 
         startActivity(intent);
     }
