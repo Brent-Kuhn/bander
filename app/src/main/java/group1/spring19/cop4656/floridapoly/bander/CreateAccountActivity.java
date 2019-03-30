8beecca7086acc50787e4a54cb92272064a1fad9
Brent <bkuhn4669@flpoly.org>
2019-03-30 12:22:26 -0400

    forgot to update the xml name during last commit

--- app/src/main/java/group1/spring19/cop4656/floridapoly/bander/CreateAccountActivity.java
+++ app/src/main/java/group1/spring19/cop4656/floridapoly/bander/CreateAccountActivity.java
@@ -26,7 +26,7 @@ public class CreateAccountActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_username_password);
+        setContentView(R.layout.create_account);
 
         // Get the Firebase auth instance
         auth = FirebaseAuth.getInstance();
