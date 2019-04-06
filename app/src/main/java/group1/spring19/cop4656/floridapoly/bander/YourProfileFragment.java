31407c204808246d31140a2fc5000fe0f8975241
Brent <bkuhn4669@flpoly.org>
2019-04-06 19:19:18 -0400

    Your Profile view now displays the current user their profile

--- app/src/main/java/group1/spring19/cop4656/floridapoly/bander/YourProfileFragment.java
+++ app/src/main/java/group1/spring19/cop4656/floridapoly/bander/YourProfileFragment.java
@@ -9,9 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -23,8 +31,21 @@ public class YourProfileFragment extends Fragment {
 
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
+    private DatabaseReference mDatabase;
     private FirebaseUser user;
     private String userId;
+    private StorageReference mStorageRef;
+
+    private TextView mLink;
+
+
+    private TextView mUserBio;
+    private TextView mUserContactInfo;
+    private TextView mUserName;
+    private TextView mInstrument;
+    private TextView mUserType;
+    private ImageView mUserImage;
+    private String image;
 
 
     @Nullable
@@ -38,7 +59,28 @@ public class YourProfileFragment extends Fragment {
         user = FirebaseAuth.getInstance().getCurrentUser();
         userId = user.getUid();
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                getData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        mUserName = (TextView) v.findViewById(R.id.bandNameSrchTextView);
+        mUserType = (TextView) v.findViewById(R.id.ArtistTypeSrchTextView);
+        mInstrument = (TextView) v.findViewById(R.id.InstrumentTextView);
+        mLink = (TextView) v.findViewById(R.id.linkTextView);
+        mUserBio = (TextView) v.findViewById(R.id.bioTextView);
+        mUserContactInfo = (TextView) v.findViewById(R.id.contactTextView);
+        mUserImage = (ImageView) v.findViewById(R.id.srchProfileImageView);
 
         authListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -73,11 +115,34 @@ public class YourProfileFragment extends Fragment {
 
     }
 
-    public void goToEditFilters() {
-        Intent intent = new Intent(getActivity(), FiltersActivity.class);
+    private void getData(DataSnapshot dataSnapshot) {
+        for(DataSnapshot ds : dataSnapshot.getChildren()) {
+            UserInfo uInfo = new UserInfo();
+            uInfo.setBio(ds.child(userId).getValue(UserInfo.class).getBio());
+            uInfo.setContact(ds.child(userId).getValue(UserInfo.class).getContact());
+            uInfo.setGenre(ds.child(userId).getValue(UserInfo.class).getGenre());
+            uInfo.setInstrument(ds.child(userId).getValue(UserInfo.class).getInstrument());
+            uInfo.setLink(ds.child(userId).getValue(UserInfo.class).getLink());
+            uInfo.setType(ds.child(userId).getValue(UserInfo.class).getType());
+            uInfo.setUsername(ds.child(userId).getValue(UserInfo.class).getUsername());
+            uInfo.setImage(ds.child(userId).getValue(UserInfo.class).getImage());
 
-        startActivity(intent);
+            mUserName.setText(uInfo.getUsername());
+            mUserType.setText(uInfo.getType());
+            mInstrument.setText(uInfo.getInstrument());
+            mLink.setText(uInfo.getLink());
+            mUserBio.setText(uInfo.getBio());
+            mUserContactInfo.setText(uInfo.getContact());
+            image = uInfo.getImage();
+            Glide.with(this).load(image).into(mUserImage);
+        }
     }
+
+//    public void goToEditFilters() {
+//        Intent intent = new Intent(getActivity(), FiltersActivity.class);
+//
+//        startActivity(intent);
+//    }
 
     public void goToEditProfile() {
         Intent intent = new Intent(getActivity(), EditProfileActivity.class);
