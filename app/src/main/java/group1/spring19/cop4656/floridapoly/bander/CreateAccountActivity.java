bc94155d0e7359417f913e27220e9f237b8b9442
Brent <bkuhn4669@flpoly.org>
2019-04-12 20:09:38 -0400

    Added location serivices to account creation, and fixed a dispaly bug in profile screen

--- app/src/main/java/group1/spring19/cop4656/floridapoly/bander/CreateAccountActivity.java
+++ app/src/main/java/group1/spring19/cop4656/floridapoly/bander/CreateAccountActivity.java
@@ -1,8 +1,15 @@
 package group1.spring19.cop4656.floridapoly.bander;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,6 +18,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -32,12 +41,19 @@ public class CreateAccountActivity extends AppCompatActivity {
     private FirebaseUser user;
     private String userId;
 
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private String longitude;
+    private String latitude;
+
     ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
+
+        checkLocationPermission();
 
         // Get the Firebase auth instance
         auth = FirebaseAuth.getInstance();
@@ -56,6 +72,9 @@ public class CreateAccountActivity extends AppCompatActivity {
 
             }
         });
+
+
+
 
         pd = new ProgressDialog(this);
         pd.setMessage("Creating Account....");
@@ -87,6 +106,7 @@ public class CreateAccountActivity extends AppCompatActivity {
                         pd.show();
                         if(!task.isSuccessful()) {
                             Toast.makeText(CreateAccountActivity.this, "Authentication failed " + task.getException(),  Toast.LENGTH_SHORT).show();
+                            pd.dismiss();
                         } else{
                             // User information
                             user = FirebaseAuth.getInstance().getCurrentUser();
@@ -119,9 +139,19 @@ public class CreateAccountActivity extends AppCompatActivity {
                                                                                             mDatabase.child("users").child(userId).child("image").setValue("https://firebasestorage.googleapis.com/v0/b/bander-98025.appspot.com/o/bander-logo.png?alt=media&token=8e38e2d9-1e0b-4177-b1a3-98c2a522bd42").addOnSuccessListener(new OnSuccessListener<Void>() {
                                                                                                 @Override
                                                                                                 public void onSuccess(Void aVoid) {
-                                                                                                    pd.dismiss();
-                                                                                                    startActivity(new Intent(CreateAccountActivity.this, EditProfileActivity.class));
-                                                                                                    finish();
+                                                                                                    mDatabase.child("users").child(userId).child("longitude").setValue(longitude).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                                                        @Override
+                                                                                                        public void onSuccess(Void aVoid) {
+                                                                                                            mDatabase.child("users").child(userId).child("latitude").setValue(latitude).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                                                                @Override
+                                                                                                                public void onSuccess(Void aVoid) {
+                                                                                                                    pd.dismiss();
+                                                                                                                    startActivity(new Intent(CreateAccountActivity.this, EditProfileActivity.class));
+                                                                                                                    finish();
+                                                                                                                }
+                                                                                                            });
+                                                                                                        }
+                                                                                                    });
                                                                                                 }
                                                                                             });
                                                                                         }
@@ -145,6 +175,76 @@ public class CreateAccountActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    public boolean checkLocationPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this).setTitle("Location Permissions").setMessage("Bander requires location services").setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(CreateAccountActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                    }
+                }).create().show();
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            } return false;
+        } else {
+            // Permission has already been granted
+            getLocation();
+
+        } return true;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    getLocation();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    goToLogin();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    public void getLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            longitude = String.valueOf(location.getLongitude());
+                            latitude = String.valueOf(location.getLatitude());
+                            Toast.makeText(getApplicationContext(), "Long: " + longitude + " Lat: " + latitude, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     public void goToLogin() {
